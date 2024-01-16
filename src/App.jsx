66b0867e29e87cbb659fa889c2e6623e244e0cd3
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2024-01-17 05:35:23 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='text-3xl font-bold text-red-200'>aaa</div>
+    </div>
+  );
+}
+
+export default App;
